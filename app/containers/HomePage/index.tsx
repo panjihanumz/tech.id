9a2b986746861e153510bf393835efa224b4b503
panhanumaks <119551768+panhanumaks@users.noreply.github.com>
2023-04-03 16:28:59 +0700

    INITIAL

---
+++ app/containers/HomePage/index.tsx
@@ -0,0 +1,17 @@
+/**
+ * NotFoundPage
+ *
+ * This is the page we show when the user visits a url that doesn't have a route
+ */
+
+import * as React from 'react';
+
+import H1 from 'components/H1';
+
+export default function HomePage() {
+  return (
+    <article>
+      <H1></H1>
+    </article>
+  );
+}
